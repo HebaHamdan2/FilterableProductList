4fe1b2ae2dbdd0970b1ba23e7e4f40f2dd352176
HebaHamdan2 <hebasaeedhamdan296@gmail.com>
2024-09-23 19:36:30 +0300

    ph2 setup footer and navbar

---
+++ react-phase-2/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+export default function Navbar() {
+  return (
+    <>
+        <nav>
+            <h1>COELACANTH</h1>
+            <h2 id="category">OUR PRODUCT LISTS</h2>
+        </nav>
+    </>
+  )
+}
